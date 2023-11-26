1639c12b40ec4021e29bf375ed7593969ceda679
Victor Borja <vborja2424@gmail.com>
2023-11-26 17:00:15 -0500

    ApiSede

--- src/main/java/pe/cibertec/ProyectoFinal/ApiSede/controller/SedeController.java
+++ src/main/java/pe/cibertec/ProyectoFinal/ApiSede/controller/SedeController.java
@@ -1,6 +1,7 @@
 package pe.cibertec.ProyectoFinal.ApiSede.controller;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,73 +14,95 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.cibertec.ProyectoFinal.ApiSede.entity.Sede;
+import pe.cibertec.ProyectoFinal.ApiSede.exception.EntityNotFoundException;
 import pe.cibertec.ProyectoFinal.ApiSede.service.SedeService;
 
 @RestController
 @RequestMapping("/api/v1/sede")
+@Slf4j
 
 public class SedeController {
-    
+
     @Autowired
-    
+
     private SedeService sedeService;
-    
+
     @GetMapping("/findAll")
-    
-    public ResponseEntity<List<Sede>> findAll(){ 
-        
-        return new ResponseEntity<>(sedeService.findAll(),HttpStatus.OK);
-        
+
+    public ResponseEntity<List<Sede>> findAll() {
+        log.info("Endpoint: /api/v1/sede/findAll - Buscando todas las sedes");
+        return new ResponseEntity<>(sedeService.findAll(), HttpStatus.OK);
+
     }
-    
+
     @GetMapping("/findByCodigoS/{codigoS}")
-    
+
     public ResponseEntity<Sede> findByCodigoS(@PathVariable Long codigoS) {
-        
-        return new ResponseEntity<>(sedeService.findByCodigoS(codigoS), HttpStatus.OK);
-        
+        try {
+            log.info("Endpoint: /api/v1/sede/findByCodigoS/{} - Buscando sede por código: {}", codigoS, codigoS);
+            Sede sede = sedeService.findByCodigoS(codigoS);
+            return new ResponseEntity<>(sede, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al buscar sede por código {}: {}", codigoS, e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-    
-    
+
     @GetMapping("/buscarPorId/{id}")
-    
+
     public ResponseEntity<Sede> buscarPorId(@PathVariable Long id) {
-        
-        return new ResponseEntity<>(sedeService.buscarPorId(id), HttpStatus.OK);
-        
+        try {
+            log.info("Endpoint: /api/v1/sede/buscarPorId/{} - Buscando sede por ID: {}", id, id);
+            Sede sede = sedeService.buscarPorId(id);
+            return new ResponseEntity<>(sede, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al buscar sede por ID {}: {}", id, e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
-    
+
     @GetMapping("/findByDistrito/{distrito}")
-    
-    public ResponseEntity<Sede> findByDistrito (@PathVariable String distrito) {
-        
-        return new ResponseEntity<>(sedeService.findByDistrito(distrito), HttpStatus.OK);
-        
+
+    public ResponseEntity<Sede> findByDistrito(@PathVariable String distrito) {
+        try {
+            log.info("Endpoint: /api/v1/sede/findByDistrito/{} - Buscando sede por distrito: {}", distrito, distrito);
+            Sede sede = sedeService.findByDistrito(distrito);
+            return new ResponseEntity<>(sede, HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al buscar sede por distrito {}: {}", distrito, e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
-    
+
     @PostMapping("/agregarSede")
-    
-    public ResponseEntity<Sede> agregarSede (@RequestBody Sede sede) {
-        
+
+    public ResponseEntity<Sede> agregarSede(@RequestBody Sede sede) {
+        log.info("Endpoint: /api/v1/sede/agregarSede - Agregando nueva sede: {}", sede);
+
         return new ResponseEntity<>(sedeService.agregarSede(sede), HttpStatus.CREATED);
-        
+
     }
-    
+
     @PutMapping("/actualizarSede")
-    
-    public ResponseEntity<Sede> actualizarSede (@RequestBody Sede sede) {
-        
+
+    public ResponseEntity<Sede> actualizarSede(@RequestBody Sede sede) {
+        log.info("Endpoint: /api/v1/sede/actualizarSede - Actualizando sede con ID {}: {}", sede.getId(), sede);
+
         return new ResponseEntity<>(sedeService.actualizarSede(sede), HttpStatus.OK);
-        
+
     }
-    
+
     @DeleteMapping("/eliminarSede/{id}")
-    
+
     public void eliminarSede(@PathVariable Long id) {
-        
-        sedeService.eliminarSede(id);
-        
+        try {
+            log.info("Endpoint: /api/v1/sede/eliminarSede/{} - Eliminando sede con ID: {}", id, id);
+            sedeService.eliminarSede(id);
+        } catch (EntityNotFoundException e) {
+            log.error("Error al eliminar sede con ID {}: {}", id, e.getMessage());
+        }
     }
-    
-    
+
 }
--- src/main/java/pe/cibertec/ProyectoFinal/ApiSede/serviceImpl/SedeServiceImpl.java
+++ src/main/java/pe/cibertec/ProyectoFinal/ApiSede/serviceImpl/SedeServiceImpl.java
@@ -1,6 +1,7 @@
 package pe.cibertec.ProyectoFinal.ApiSede.serviceImpl;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.cibertec.ProyectoFinal.ApiSede.dao.SedeRepository;
@@ -9,6 +10,7 @@ import pe.cibertec.ProyectoFinal.ApiSede.exception.EntityNotFoundException;
 import pe.cibertec.ProyectoFinal.ApiSede.service.SedeService;
 
 @Service
+@Slf4j
 
 public class SedeServiceImpl implements SedeService {
 
@@ -18,13 +20,14 @@ public class SedeServiceImpl implements SedeService {
 
     @Override
     public List<Sede> findAll() {
-
+        log.info("Buscando Sede");
         return (List<Sede>) sedeRepository.findAll();
 
     }
 
     @Override
     public Sede buscarPorId(Long id) {
+        log.info("Buscando sede por ID: {}", id);
 
         return sedeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Sede no encontrada por el id" + id.toString()));
 
@@ -32,6 +35,7 @@ public class SedeServiceImpl implements SedeService {
 
     @Override
     public Sede findByDistrito(String distrito) {
+        log.info("Buscando sede por distrito: {}", distrito);
 
         return sedeRepository.findByDistrito(distrito).orElseThrow(() -> new EntityNotFoundException("Distrito no encontrado con el nombre" + distrito.toString()));
 
@@ -39,6 +43,7 @@ public class SedeServiceImpl implements SedeService {
 
     @Override
     public Sede agregarSede(Sede sede) {
+        log.info("Agregando nueva sede: {}", sede);
 
         return sedeRepository.save(sede);
 
@@ -46,6 +51,7 @@ public class SedeServiceImpl implements SedeService {
 
     @Override
     public Sede actualizarSede(Sede sede) {
+        log.info("Actualizando sede con ID {}: {}", sede.getId(), sede);
 
         var SedeF = sedeRepository.findById(sede.getId()).get();
         SedeF.setCodigoS(sede.getCodigoS());
@@ -61,18 +67,19 @@ public class SedeServiceImpl implements SedeService {
 
     @Override
     public void eliminarSede(Long id) {
+        log.info("Eliminando sede con ID: {}", id);
 
         var SedeF = sedeRepository.findById(id).get();
-
         sedeRepository.delete(SedeF);
 
     }
 
     @Override
     public Sede findByCodigoS(Long codigoS) {
-        
-        return sedeRepository.findByCodigoS(codigoS).orElseThrow(() -> new EntityNotFoundException("Sede no encontrado con el codigo"+codigoS.toString()));
-        
+        log.info("Buscando sede por código: {}", codigoS);
+
+        return sedeRepository.findByCodigoS(codigoS).orElseThrow(() -> new EntityNotFoundException("Sede no encontrado con el codigo" + codigoS.toString()));
+
     }
 
 }
